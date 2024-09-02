b8f6f9ba717ecbaeb8d8e62876716e1a8be56d76
Mohamed Mohideen Thayub <mthayub@freshworks.com>
2024-09-02 04:35:57 -0700

    initial commit

---
+++ intrest.js
@@ -0,0 +1,5 @@
+var amount = 1000;
+var rate = 5; 
+var year = 3; 
+var final = Math.trunc(amount * rate * year / 100); 
+console.log(final);
---
+++ multiple_five.js
@@ -0,0 +1,9 @@
+function check(num){
+    if (num % 5 == 0){
+        return true
+    }
+    else{
+        return false
+    }
+}
+console.log(check(55))
